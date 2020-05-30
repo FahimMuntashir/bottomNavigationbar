932d71d4d60a93bc81954653cc1629ed781c80cb
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-31 01:58:19 +0600

    scaffold

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,21 +1,37 @@
 import 'package:flutter/material.dart';
 
-
-
 class ScaffoldExam extends StatelessWidget {
+
+
+  _iconSend(){
+    debugPrint('hello clockd');
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-
-        title: Text('Bottom Navbar'),
+        title: Text(
+          'Bottom Navbar',
+          style: TextStyle(color: Colors.black),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.tealAccent,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => debugPrint('email send'),
+          ),
+          IconButton(
+            icon: Icon(Icons.email),
+            onPressed: _iconSend ,
+          ),
+        ],
       ),
+      backgroundColor: Colors.lightBlue,
+      body: Center(child: Text('I\'m body'),),
     );
   }
 }
-
-
 
 class Home extends StatelessWidget {
   @override
