8cf42346069fa931128d8265dd8c422678cb9f98
FahimMuntashir <fahim.tech.me@gmail.com>
2020-06-01 01:31:01 +0600

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
 
-import 'ui/home.dart';
+void main() => runApp(MyApp());
 
-//void main() => runApp(Home());
-
-
-void main() => runApp( MaterialApp(
-  home: ScaffoldExam(),
-
-));
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('BottomNavBar')),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.add), title: Text('First')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: Text('Second')),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle), title: Text('Third')),
+          ],
+        ),
+      ),
+    );
+  }
+}
